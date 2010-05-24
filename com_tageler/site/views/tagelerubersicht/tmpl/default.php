1f5278a24a839f8069c02a0e89495b1de0825d63
Robin Baumgartner <robin@baumgartners.ch>
2010-05-24 18:54:10 +0200

    tabellenübersicht formatiert

--- com_tageler/site/views/tagelerubersicht/tmpl/default.php
+++ com_tageler/site/views/tagelerubersicht/tmpl/default.php
@@ -8,19 +8,55 @@ function date_mysql2german($date)
     
     return    sprintf("%02d.%02d.%04d", $d[2], $d[1], $d[0]);
 }
+?>
 
-echo "<h1>Tageler-Übersicht für ".date_mysql2german($this->allTageler[0]->datum)."</h1>";
-echo "<table>";
-echo "<tr><td>Einheit</td><td>Titel</td><td>Beginn</td><td>Schluss</td></tr>";
+<style type='text/css'>
+    table.tageler {
+        font: 11px/24px Verdana, Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width:100%;
+        }
 
-foreach($this->allTageler as $tageler)
-{
-    echo "<tr>";
-    echo "<td><a href='index.php?option=com_tageler&view=tageler&einheit=".$tageler->einheit."'>".ucwords($tageler->einheit)."</a></td>";
-    echo "<td>".$tageler->titel."</td>";
-    echo "<td>".$tageler->beginn."</td>";
-    echo "<td>".$tageler->schluss."</td>";
-    echo "</tr>";
-}
+    tr.header td {
+        background: #006f0d;
+        color:white;
+        border-top: 1px solid #CCC;
+        font-weight:bold;
+        }
 
-echo "</table>";
+    td.adjacent {
+        border-left: 1px solid #CCC;
+        border-right: 1px solid #CCC;
+        border-bottom: 1px solid #CCC;
+        padding: 0 0.5em;
+        }
+</style>
+
+<div class="componentheading">
+    Tagelerübersicht
+</div>
+<table class='tageler'>
+    <tr class="header">
+        <td class='adjacent'>Einheit</td>
+        <td class='adjacent'>Titel</td>
+        <td class='adjacent'>Beginn</td>
+        <td class='adjacent'>Schluss</td>
+    </tr>
+    <tr><td colspan='4'></td></tr>
+    <?php
+    foreach($this->allTageler as $tageler)
+    {
+    ?>
+    <tr>
+        <td class='adjacent'>
+            <a href='index.php?option=com_tageler&view=tageler&einheit=".$tageler->einheit."'>
+                <?php echo ucwords($tageler->einheit); ?>
+            </a>
+        </td>
+        <td class='adjacent'><?php echo $tageler->titel; ?></td>
+        <td class='adjacent'><?php echo $tageler->beginn; ?></td>
+        <td class='adjacent'><?php echo $tageler->schluss; ?></td>
+    </tr>
+    <?php
+    } ?>
+</table>
