1b98cf819943e182fb8f287e3f2acb9c84239585
Robin Baumgartner <robin@baumgartners.ch>
2010-07-28 14:37:56 +0200

    Verbesserung der Ladegeschwindigkeit

--- mod_tageler/tmpl/default.php
+++ mod_tageler/tmpl/default.php
@@ -6,14 +6,14 @@ foreach ($tageler as $t)
 {
 	if ($t->datum >= date("Y-m-d"))
 	{
-		echo "<img src='modules/mod_tageler/img/new.png' alt='new' style='margin-right:10px' />";
+		echo '<img src="modules/mod_tageler/img/new.png" width="9" height="9" alt="new" style="margin-right:10px" />';
 	}
 	else
 	{
-		echo "<img src='modules/mod_tageler/img/old.png' alt='old' style='margin-right:10px' />";
+		echo '<img src="modules/mod_tageler/img/old.png" width="9" height="9" alt="old" style="margin-right:10px" />';
 	}
 
-	echo "<a href='index.php?option=com_tageler&controller=tageler_detail&cid[]=".$t->einheit."'>".$t->name."</a><br />";
+	echo '<a href="index.php?option=com_tageler&controller=tageler_detail&cid[]='.$t->einheit.'">'.$t->name.'</a><br />';
 }
 
-echo "<div style='margin-left:20px'><a href='index.php?option=com_tageler'>Alle Tageler</a></div><br />";
+echo '<div style="margin-left:20px"><a href="index.php?option=com_tageler">Alle Tageler</a></div><br />';
