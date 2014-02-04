169f12e835d4a73c992e24f8556db039e8efea21
Robin Baumgartner <robin@baumgartners.ch>
2014-02-04 22:50:35 +0100

    Added JSON format for tageler views

---
+++ com_tageler/site/views/tageler/view.json.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * JSON Tageler View für das Tageler Component
+ * 
+ * @package    Falkenstein.Joomla
+ * @subpackage Components
+ * @link       http://www.pfadi-falkenstein.ch
+ * @license		GNU/GPL
+ */
+
+// Import der Basisklasse
+jimport( 'joomla.application.component.view');
+
+/**
+ * JSON Viewklasse für die Tagelerübersicht
+ *
+ * @package		Falkenstein.Joomla
+ * @subpackage	Components
+ */
+class TagelerViewTageler extends JView
+{
+    function display($tpl = null)
+    {
+        // Daten laden
+        $model = $this->getModel();
+        $allTageler = $model->getTageler();
+
+        $document =& JFactory::getDocument();
+        $document->setMimeEncoding('application/json');
+
+        echo json_encode($allTageler);
+    }
+}
---
+++ com_tageler/site/views/tageler_detail/view.json.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * JSON Tageler View für das Tageler Component
+ * 
+ * @package    Falkenstein.Joomla
+ * @subpackage Components
+ * @link       http://www.pfadi-falkenstein.ch
+ * @license		GNU/GPL
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+// Import der Basisklasse
+jimport( 'joomla.application.component.view');
+
+/**
+ * JSON Viewklasse für die Tageler.
+ *
+ * @package		Falkenstein.Joomla
+ * @subpackage	Components
+ */
+class Tageler_DetailViewTageler_Detail extends JView
+{
+    function display($tpl = null)
+    {
+        // Daten laden
+        $array = JRequest::getVar('cid',  0, '', 'array');
+        $einheit = $array[0];
+
+        $model    = $this->getModel();
+        $tageler  = $model->getTageler($einheit);
+        $felder   = $model->getFelder($einheit);
+        $abtInfos = $model->getAbtInfos();
+
+        $document =& JFactory::getDocument();
+        $document->setMimeEncoding('application/json');
+
+        $tageler->extra = $felder;
+        $tageler->abtInfos = $abtInfos;
+
+        echo json_encode($tageler);
+    }
+}
